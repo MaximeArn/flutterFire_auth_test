591928ee84db8c71c8534fb746edff2edc55573f
MaximeArn <maxime-arnould@outlook.fr>
2022-01-04 17:26:50 +0100

    show snackbar on sign out if no user loged before

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -12,8 +12,8 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Auth test"),
-        actions: const <Widget>[
-          SignedOutBtn(),
+        actions:  <Widget>[
+          SignedOutBtn(auth: auth,),
         ],
       ),
       body: const Center(
--- lib/widgets/signed_out_btn.dart
+++ lib/widgets/signed_out_btn.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignedOutBtn extends StatelessWidget {
-  const SignedOutBtn({Key? key}) : super(key: key);
-
-  void logOut() => {
-    print("hello")
-  };
+  final FirebaseAuth auth;
+  const SignedOutBtn({Key? key, required this.auth}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    void logOut() async {
+      final User? user = auth.currentUser;
+      if (user == null) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('No one has signed in.'),
+        ));
+        return; 
+      }
+    }
+
     return TextButton(
       child: const Text('Sign out'),
       style: TextButton.styleFrom(primary: Colors.white),
