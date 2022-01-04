2f1a3977e7dc183151252fe1825b9850f31a3201
MaximeArn <maxime-arnould@outlook.fr>
2022-01-04 15:26:08 +0100

    configure project

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# flutterfire_auth
+# **flutterfire_auth**
 
 Project to set a full authentication system in a flutter app
+
+## **Prerequisites**
+
+install `firebase_core` that is responsible to connect the app to firebase services
+
+Activate the `flutterfire_cli` ==> `dart pub global activate flutterfire_cli`
+
+configure the application for using firebase services declared in the firebase project ==> `flutterfire configure`
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutterfire_auth/widgets/home.dart';
 
 void main() {
@@ -10,13 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
