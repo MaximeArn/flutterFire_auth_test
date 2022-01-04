5b66a1cfdf73d8e86e1af05779f7d0b13b384ae3
MaximeArn <maxime-arnould@outlook.fr>
2022-01-04 18:01:31 +0100

    create the UI form to register

---
+++ lib/widgets/email_registration.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class EmailRegistration extends StatefulWidget {
+  final FirebaseAuth auth;
+  const EmailRegistration({Key? key, required this.auth}) : super(key: key);
+
+  @override
+  _EmailRegistrationState createState() => _EmailRegistrationState();
+}
+
+class _EmailRegistrationState extends State<EmailRegistration> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _success = false;
+  late String? _userEmail;
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Registration"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              validator: (String? value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              validator: (String? value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              alignment: Alignment.center,
+              child: ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    //code function that will register the user 
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(_success == null
+                  ? ''
+                  : (_success
+                      ? 'Successfully registered ' + _userEmail.toString()
+                      : 'Registration failed')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -5,19 +5,23 @@ import 'package:flutterfire_auth/widgets/signed_out_btn.dart';
 class HomePage extends StatelessWidget {
   final FirebaseAuth auth;
 
-  const HomePage({Key? key,  required this.auth}) : super(key: key);
+  const HomePage({Key? key, required this.auth}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Auth test"),
-        actions:  <Widget>[
-          SignedOutBtn(auth: auth,),
+        actions: <Widget>[
+          SignedOutBtn(
+            auth: auth,
+          ),
         ],
       ),
-      body: const Center(
-        child: Text("HomePage"),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.all(16),
+        children: const <Widget>[],
       ),
     );
   }
