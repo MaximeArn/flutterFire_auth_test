968f3cda4b9dbf3c479edc0427ce0e75dd38029b
MaximeArn <maxime-arnould@outlook.fr>
2022-01-25 22:32:24 +0100

    simplify routes using only routes and not onGenerateRoute

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutterfire_auth/types/authentication.dart';
 import 'package:flutterfire_auth/widgets/auth_form.dart';
 import 'package:flutterfire_auth/widgets/connected_view.dart';
 import 'package:flutterfire_auth/widgets/not_connected_view.dart';
-
+import 'package:flutterfire_auth/widgets/verify_email.dart';
 import 'firebase_options.dart';
 import "package:firebase_core/firebase_core.dart";
 import 'package:flutter/material.dart';
@@ -37,11 +37,11 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     _sub = FirebaseAuth.instance.authStateChanges().listen((user) {
-      if(user != null ) print(user.emailVerified);
-      user != null
-          ? popAndRemplace()
-          : _navigatorKey.currentState!
-              .pushReplacementNamed(NotConnected.routeName);
+      if (user != null) print(user.emailVerified);
+      user == null
+          ? _navigatorKey.currentState!
+              .pushReplacementNamed(NotConnected.routeName)
+          : popAndRemplace();
     });
   }
 
@@ -53,13 +53,12 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       navigatorKey: _navigatorKey,
       title: 'Firebase Auth Intro',
-      initialRoute: FirebaseAuth.instance.currentUser == null
-          ? NotConnected.routeName
-          : Connected.routeName,
+      initialRoute: "/",
       onGenerateRoute: (RouteSettings settings) {
         switch (settings.name) {
           case Connected.routeName:
@@ -82,11 +81,15 @@ class _MyAppState extends State<MyApp> {
         }
       },
       routes: {
+        "/": (_) => const Scaffold(body: Center(child: CircularProgressIndicator(),),),
         "loginForm": (_) => const AuthenticationForm(
               method: AuthenticationMethod.connection,
             ),
         "registerForm": (_) =>
             const AuthenticationForm(method: AuthenticationMethod.registration),
+        VerifyEmail.routeName: (_) => const VerifyEmail(),
+        NotConnected.routeName: (_) => const NotConnected(),
+        Connected.routeName: (_) => const Connected(),
       },
     );
   }
--- lib/widgets/verify_email.dart
+++ lib/widgets/verify_email.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
 class VerifyEmail extends StatelessWidget {
-  const VerifyEmail({ Key? key }) : super(key: key);
+  static const routeName = "verifyEmail";
+  const VerifyEmail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    void sendNewVerificationLink(){
-
+    void sendNewVerificationLink() {
+      print("send new verification link");
     }
-    return Center(
-      child: Column(
-        children:  [
-          const Text("You must verify your email to access the rest of the app"),
-          ElevatedButton(onPressed: sendNewVerificationLink, child: const Text("Send new link")) 
-        ],
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Authentication"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+                "You must verify your email to access the rest of the app"),
+            ElevatedButton(
+                onPressed: sendNewVerificationLink,
+                child: const Text("Send new link"))
+          ],
+        ),
       ),
     );
   }
