0bf7cdaf4aad934646db98edb0d196e2fe80f1a2
MaximeArn <maxime-arnould@outlook.fr>
2022-02-12 18:00:31 +0100

    code the UIof the ForgotPassword Page

--- lib/widgets/auth/widgets/forgot_password.dart
+++ lib/widgets/auth/widgets/forgot_password.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
 class ForgotPassword extends StatefulWidget {
@@ -8,12 +9,63 @@ class ForgotPassword extends StatefulWidget {
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
+  Future<void> verifyEmail() async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         title: const Text("Forgot Password"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Receive an email to\nreset your password.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: emailController,
+                cursorColor: Colors.white,
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(labelText: 'Email'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (email) =>
+                    email != null && !EmailValidator.validate(email)
+                        ? 'Enter a valid email'
+                        : null,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size.fromHeight(50),
+                ),
+                icon: const Icon(Icons.email_outlined),
+                label: const Text(
+                  'Reset Password',
+                  style: TextStyle(fontSize: 24),
+                ),
+                onPressed: verifyEmail,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
